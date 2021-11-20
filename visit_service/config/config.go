83a9d6cf475b414b90d1d50e6c37a4e3266b4569
andrewd92 <adovgun92@gmail.com>
2021-11-20 01:06:00 +0100

    make all services as module

--- visit_service/config/config.go
+++ visit_service/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+	"github.com/spf13/viper"
 	"gopkg.in/yaml.v2"
 	"os"
 	"path"
@@ -48,4 +50,20 @@ func Instance() Config {
 	}
 
 	return *config
+}
+
+func Init() {
+	viper.SetConfigName("config")          // name of config file (without extension)
+	viper.SetConfigType("yaml")            // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("./")              // path to look for the config file in
+	viper.AddConfigPath("./visit_service") // path to look for the config file in
+	viper.AddConfigPath("$HOME/config")    // call multiple times to add many search paths
+	viper.AddConfigPath(".")               // optionally look for config in the working directory
+
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
 }
--- visit_service/server/server.go
+++ visit_service/server/server.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"github.com/andrewd92/timeclub/visit_service/config"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"log"
+	"os"
 )
 
 var (
@@ -13,8 +16,17 @@ var (
 func StartApplication() {
 	mapUrls()
 
-	err := router.Run(":" + config.Instance().Server.Port.Http)
+	config.Init()
+
+	port := viper.GetString("server.port.http")
+
+	log.Printf("ENV port :%s", viper.GetString("SERVER_PORT_HTTP"))
+	fmt.Println("ENV: ", os.Getenv("SERVER_PORT_HTTP"))
+
+	err := router.Run(":" + port)
 	if err != nil {
 		fmt.Println("Err: ", err.Error())
 	}
+
+	log.Printf("Server run on port :%s", port)
 }
